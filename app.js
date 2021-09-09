d43562fc112fb10ca6f1fd71438b43d16dc13dad
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-09-09 10:22:46 -0700

    Complete exercise

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+'use strict'
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let submitbtn = document.getElementById('submit');
+
+submitbtn.addEventListener('click', () => {
+  let apiImage = document.getElementById('gif');
+  let userInput = document.getElementById("type-of-gif").value;
+  let apiKey = 'F3PJP0CSLafPf720AJMNpaqYSL3T4oAA';
+
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${userInput}`, { mode: "cors" })
+    .then((res) => {return res.json()})
+    .then((response) => {
+      console.log(response);
+      apiImage.src = response.data.images.original.url;
+    }).catch((err) => {
+    console.log(err);
+    });
+});
